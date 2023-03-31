53bba5d0170ab42ebc0872a627d11e131257492f
naufalbayazid <naufalichwanbayazid@gmail.com>
2023-03-31 11:21:06 +0700

    menampilkan otput

--- quicksortt_192/quicksortt_192.cpp
+++ quicksortt_192/quicksortt_192.cpp
@@ -5,7 +5,7 @@ int arr[20];
 int cmp_count = 0;
 int mov_count = 0;
 int n;
-void input () {
+void input() {
 	while (true)
 	{
 		cout << "masukkan panjang element array ;";
@@ -14,4 +14,11 @@ void input () {
 		if (n <= 20)
 			break;
 		else
-			cout << "\n masukkan panjang array adalah 20"
+			cout << "\n masukkan panjang array adalah 20" << endl;
+
+	}
+	cout << "\n===============" << endl;
+	cout << "\nenter array element" << endl;
+	cout << "\n===============" << endl;
+
+
