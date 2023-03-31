ddfd6cd89c6905ec79384376269977be00c7999e
naufalbayazid <naufalichwanbayazid@gmail.com>
2023-03-31 11:16:28 +0700

    memberikan nama void input

--- quicksortt_192/quicksortt_192.cpp
+++ quicksortt_192/quicksortt_192.cpp
@@ -5,3 +5,13 @@ int arr[20];
 int cmp_count = 0;
 int mov_count = 0;
 int n;
+void input () {
+	while (true)
+	{
+		cout << "masukkan panjang element array ;";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "\n masukkan panjang array adalah 20"
