41117db11aa24b02a03e259e6b2523363c9bbce0
naufalbayazid <naufalichwanbayazid@gmail.com>
2023-03-31 11:25:09 +0700

    menambahkan void input

--- quicksortt_192/quicksortt_192.cpp
+++ quicksortt_192/quicksortt_192.cpp
@@ -21,4 +21,10 @@ void input() {
 	cout << "\nenter array element" << endl;
 	cout << "\n===============" << endl;
 
+	for (int i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << ">";
+		cin >> arr[i]
+	}
+} 
 
